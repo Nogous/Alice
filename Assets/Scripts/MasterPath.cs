a9b8587b5df3bda75c9c7d9e88464bfa15402084
Camille <camille.lavarde@gmail.com>
2020-09-22 18:32:36 +0200

    change path

---
+++ Assets/Scripts/MasterPath.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterPath : MonoBehaviour
+{
+    public static MasterPath instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.Log("l'instance de MasterPath existe deja. :)");
+            Destroy(gameObject);
+        }
+    }
+
+    public PathCreation.PathCreator mainPath;
+    public PathCreation.Examples.PathFollower playerPath;
+
+    public PathCreation.PathCreator[] paths;
+
+    public void SwitchMainPath(int id)
+    {
+        mainPath = paths[id];
+
+        playerPath.SetPathCreator(mainPath);
+        playerPath.SetDistanceInPath(0);
+    }
+}
