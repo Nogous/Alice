b99a3b640f327146c8ccb9bd39f14a37c68424f4
Camille <camille.lavarde@gmail.com>
2020-09-22 15:30:48 +0200

    life and speed

--- Assets/Scripts/PlayerEntity.cs
+++ Assets/Scripts/PlayerEntity.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class PlayerEntity : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    [Header("Speed")]
+    public float initMoveSpeed = 5f;
+    public float moveSpeedMax = 10f;
+    public float moveSpeedMin = 1f;
+    private float currentSpeed = 5f;
+
+    [Header("life")]
+    public int life = 5;
+    public float invicibilityFrameDuration = .1f;
+    private bool isInvincible = false;
 
     private Vector3 movement;
+
+    private void Start()
+    {
+        currentSpeed = initMoveSpeed;
+    }
 
     private void Update()
     {
@@ -18,6 +32,43 @@ public class PlayerEntity : MonoBehaviour
     {
         if (movement == Vector3.zero) return;
 
-        transform.localPosition += movement * Time.deltaTime * moveSpeed;
+        transform.localPosition += movement * Time.deltaTime * initMoveSpeed;
+    }
+
+    private void TakeDamage(int dammage)
+    {
+        life -= dammage;
+        if (life<= 0)
+        {
+            GameManager.instance.PlayerDead();
+        }
+        isInvincible = true;
+        StartCoroutine(InvincibilityStop());
+    }
+
+    #region speed
+    public void SpeedDown(float amont)
+    {
+        currentSpeed -= amont;
+        if (currentSpeed < moveSpeedMin)
+        {
+            currentSpeed = moveSpeedMin;
+        }
+    }
+
+    public void SpeedUp(float amont)
+    {
+        currentSpeed += amont;
+        if (currentSpeed > moveSpeedMax)
+        {
+            currentSpeed = moveSpeedMax;
+        }
+    }
+    #endregion
+
+    private IEnumerator InvincibilityStop()
+    {
+        yield return new WaitForSeconds(invicibilityFrameDuration);
+        isInvincible = false;
     }
 }
