89c946ed1cc5d3da215f568a2df64c30d72d055f
Camille <camille.lavarde@gmail.com>
2020-09-22 14:31:10 +0200

    move player

---
+++ Assets/Scripts/PlayerEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEntity : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Vector3 movement;
+
+    private void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (movement == Vector3.zero) return;
+
+        transform.localPosition += movement * Time.deltaTime * moveSpeed;
+    }
+}
