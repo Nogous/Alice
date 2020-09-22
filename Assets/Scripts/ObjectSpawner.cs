5283b19647b81028c6b8b999a99f2c6671b6148a
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-09-22 14:11:41 +0200

    [FEAT] Spawn random obstacles at random position and rotation

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+
+    private ObjectPooler objectPooler;
+
+    [Header("Parameters")]
+    [SerializeField] private float minTimeBetweenObstacles;
+    [SerializeField] private float maxTimeBetweenObstacles;
+    [SerializeField] private string[] obstacleTags;
+    [SerializeField] private int minX;
+    [SerializeField] private int maxX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectPooler = ObjectPooler.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.K))
+        {
+            StartCoroutine(SpawnObjects());
+        }
+    }
+
+    private IEnumerator SpawnObjects()
+    {
+        objectPooler.SpawnFromPool(obstacleTags[Random.Range(0, obstacleTags.Length)], 
+            new Vector3(Random.Range(minX, maxX), 15, 0), 
+            new Vector3(Random.Range(-90, 90), Random.Range(-90, 90), Random.Range(-90, 90)));
+        yield return new WaitForSeconds(Random.Range(minTimeBetweenObstacles, maxTimeBetweenObstacles));
+        StartCoroutine(SpawnObjects());
+    }
+}
