248d41ae7518e4b311f1bb3d2718f2ddb39a138e
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-09-23 08:31:07 +0200

    [FEAT] Added AudioManager, but it's empty

---
+++ Assets/Scripts/SoundEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SoundEffect
+{
+    public string clipName;
+
+    public AudioClip[] clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(0f, 256f)]
+    public int priority;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
