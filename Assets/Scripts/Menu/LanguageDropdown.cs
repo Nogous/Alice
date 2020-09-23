eac8c9d5da7bc8fba07d4df61e272559670a19da
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-09-23 17:03:10 +0200

    [FEAT] Select language in menu

---
+++ Assets/Scripts/Menu/LanguageDropdown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageDropdown : MonoBehaviour
+{
+
+    private Dropdown drop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        drop = GetComponent<Dropdown>();
+        drop.value = ValueHolder.instance.gameLanguageId;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
