e120145f4d4b5894f8ea7a9f7f34c8a9dfe0eef4
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-09-23 18:10:33 +0200

    [FEAT] Added music, smooth transition is set up but not implemented

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -10,6 +10,8 @@ public class AudioManager : MonoBehaviour
     
     public SoundEffect[] soundEffects;
 
+    private IEnumerator coroutineToStop;
+
     private void Awake()
     {
         if (instance != null && instance != this)
@@ -28,6 +30,22 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        MiniGameManager.instance.onChangeState += () =>
+        {
+            if(MiniGameManager.instance.state == State.NONE)
+            {
+                Play("Music", 0, false);
+            }
+            else if (MiniGameManager.instance.state == State.FIRSTMG)
+            {
+                StartCoroutine(FadeOutBeforeNewMusic("Music", "Music", 1));
+            }
+        };
+        Play("Music", 0, false);
+    }
+
     public void Play(string name)
     {
         SoundEffect fx = Array.Find(soundEffects, sound => sound.clipName == name);
@@ -40,6 +58,24 @@ public class AudioManager : MonoBehaviour
         fx.source.Play();
     }
 
+    public void Play(string name, int index, bool shouldFlowWithFirstMusic)
+    {
+        SoundEffect fx = Array.Find(soundEffects, sound => sound.clipName == name);
+        if (fx == null)
+        {
+            Debug.Log("/!\\ Sound : " + name + "not found /!\\");
+            return;
+        }
+        float temp = fx.source.time;
+        fx.source.clip = fx.clip[index];
+        if (shouldFlowWithFirstMusic)
+        {
+            fx.source.time = temp;
+        }
+        fx.source.Play();
+    }
+
+
     public void Stop(string name)
     {
         SoundEffect fx = Array.Find(soundEffects, sound => sound.clipName == name);
@@ -50,4 +86,41 @@ public class AudioManager : MonoBehaviour
         }
         fx.source.Stop();
     }
+
+    public IEnumerator FadeOutBeforeNewMusic(string sourceToLower, string music, int index)
+    {
+        AudioSource fxSource = Array.Find(soundEffects, sound => sound.clipName == sourceToLower).source;
+        if (fxSource.volume - 0.2f >= 0)
+            fxSource.volume -= 0.2f;
+        yield return new WaitForSeconds(0.3f);
+        if(fxSource.volume <= 0.2f)
+        {
+            fxSource.volume = 0;
+            StopCoroutine(FadeOutBeforeNewMusic(sourceToLower, music, index));
+            Play(music, index, false);
+            StartCoroutine(FadeInNewMusic("Music"));
+        }
+        else
+        {
+            StartCoroutine(FadeOutBeforeNewMusic(sourceToLower, music, index));
+        }
+    }
+
+    private IEnumerator FadeInNewMusic(string sourceToUp)
+    {
+        AudioSource fxSource = Array.Find(soundEffects, sound => sound.clipName == sourceToUp).source;
+        if (fxSource.volume + 0.2f <= 1)
+            fxSource.volume += 0.2f;
+        yield return new WaitForSeconds(0.3f);
+        if (fxSource.volume >= 1)
+        {
+            fxSource.volume = 1;
+            StopCoroutine(FadeInNewMusic(sourceToUp));
+        }
+        else
+        {
+            StartCoroutine(FadeInNewMusic(sourceToUp));
+        }
+    }
+
 }
