5d7c891fcd56f7261759f971297615fa26f32ada
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-09-24 12:01:01 +0200

    [FEAT] First build

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
 
     public bool onDebugMode;
 
+    public System.Action<int> onPhaseChange;
+
     private void Awake()
     {
         instance = this;
@@ -17,7 +19,30 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        DialogueReader.instance.CheckDialogue("BLA", 1);
+
+        onPhaseChange += (int stateId) =>
+        {
+            switch (stateId)
+            {
+                case 1:
+                    MiniGameManager.instance.ChangeState(State.TUTO);
+                    break;
+                case 2:
+                    DialogueReader.instance.CheckDialogue("FMG", 1);
+                    break;
+                case 3:
+                    MiniGameManager.instance.ChangeState(State.FIRSTMG);
+                    break;
+                case 4:
+                    DialogueReader.instance.CheckDialogue("SMG", 1);
+                    break;
+                case 5:
+                    MiniGameManager.instance.ChangeState(State.THIRDMG);
+                    break;
+            }
+        };
+
     }
 
     // Update is called once per frame
@@ -28,6 +53,6 @@ public class GameManager : MonoBehaviour
 
     public void PlayerDead()
     {
-
+        
     }
 }
