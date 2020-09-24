66672bfa210a91420c112ace7d7b0a741c8fd3ef
Yekonori <yannisdemacedo@gmail.com>
2020-09-24 20:24:55 +0200

    Merge LifeUI

--- Assets/Scripts/TimerScore.cs
+++ Assets/Scripts/TimerScore.cs
@@ -58,4 +58,9 @@ public class TimerScore : MonoBehaviour
     {
         return _currentTimer;
     }
+
+    public void SetActive(bool isActive)
+    {
+        gameObject.SetActive(isActive);
+    }
 }
