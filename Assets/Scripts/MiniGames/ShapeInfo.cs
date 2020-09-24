62e955aa2cbdeaa8f084256ca96b8ef118b272ac
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-09-24 08:56:10 +0200

    [FEAT] Minigame 3 set up

---
+++ Assets/Scripts/MiniGames/ShapeInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeInfo : MonoBehaviour
+{
+
+    public bool loop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
