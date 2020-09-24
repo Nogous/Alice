c0b7714d3a08a5d07316433770fc985a7335a014
Camille <camille.lavarde@gmail.com>
2020-09-24 19:05:49 +0200

    improuve Shape

--- Assets/Scripts/MiniGames/ThirdMiniGameTrigger.cs
+++ Assets/Scripts/MiniGames/ThirdMiniGameTrigger.cs
@@ -7,16 +7,29 @@ public class ThirdMiniGameTrigger : MonoBehaviour
 
     [SerializeField] private int id = 0;
 
+    public GameObject particulesActive;
+    public GameObject particulesUnactive;
+
     private void Start()
     {
         id = transform.GetSiblingIndex() - 1;
+        particulesUnactive.SetActive(true);
+        particulesActive.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            particulesUnactive.SetActive(false);
+            particulesActive.SetActive(true);
             ThirdMiniGame.instance.CheckIfCanActivateTrigger(id);
         }
+    }
+
+    public void ResetParticules()
+    {
+        particulesUnactive.SetActive(true);
+        particulesActive.SetActive(false);
     }
 }
